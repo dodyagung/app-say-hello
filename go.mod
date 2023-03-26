0053313cafe0b6784f89b8587994a98c23a2e277
Dody <dodyags@gmail.com>
2023-03-26 16:44:26 +0700

    learn package

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dodyagung/app-say-hello
+
+go 1.20
+
+require github.com/dodyagung/go-say-hello v1.5.0
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/dodyagung/go-say-hello"
+)
+
+func main() {
+	fmt.Println(go_say_hello.SayHello())
+}
